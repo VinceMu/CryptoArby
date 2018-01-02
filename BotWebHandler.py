7c565fed9aa469e68e15f384af2eb44503bc1467
VinceMu <vince1997mu@gmail.com>
2018-01-02 16:40:26 +1100

    added gnicorn as deployment agent

--- BotWebHandler.py
+++ BotWebHandler.py
@@ -1,7 +1,7 @@
 from flask import  Flask
 import FlipperBot
 
-bot = FlipperBot.Flipper("BTC-ZEC","f9e36fd45e184e3c8801188dd93c4628",0.001,BuyMargin=35,SellMargin=65)
+bot = FlipperBot.Flipper("BTC-ADA","f9e36fd45e184e3c8801188dd93c4628",0.001,BuyMargin=35,SellMargin=65)
 app = Flask(__name__)
 
 @app.route('/')
